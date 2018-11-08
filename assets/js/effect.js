4c413e134834fead174493791dbc1391f661cab2
tam <taam.tr17@gmail.com>
2018-11-08 15:23:58 -0800

    Fixed the options-buttons and boxes, setInterval for bg images

--- assets/js/effect.js
+++ assets/js/effect.js
@@ -1,7 +1,26 @@
+var i=0
+var myInterval = setInterval(function () {
+    
+    var bgImages = ["boat.jpg", "cali-beach.jpg", "dining-fisherman.jpg", "longbeach.jpg", "muir-wood.jpg", "palace-of-fine-art.jpg", "sf-city.jpg", "sf-night.jpg", "sf-street.jpg", "sunrise.jpg"];
+    // var bgImgValue = bgImages[Math.floor(Math.random() * bgImages.length)];
+    bgImgValue=bgImages[i]
+    i++;
+    if(i>=bgImages.length){i=0}
+    var urlVal = `url(assets/images/${bgImgValue})`
+    $("body").css({
+        "background-image": urlVal, "background-size": "cover",
+        "background-attachment": "fixed", "background-position": "center", "background-repeat": "no-repeat", "background-color": "rgba(0, 0, 0, 0.3)", "background-blend-mode": "overlay",
+        "transition":" all 1s ease-out"
+    });
+
+}, 5000)
+
 
 var bgImages = ["boat.jpg", "cali-beach.jpg", "dining-fisherman.jpg", "longbeach.jpg", "muir-wood.jpg", "palace-of-fine-art.jpg", "sf-city.jpg", "sf-night.jpg", "sf-street.jpg", "sunrise.jpg"];
 var bgImgValue = bgImages[Math.floor(Math.random() * bgImages.length)];
 var urlVal = `url(assets/images/${bgImgValue})`
-$("body").css({ "background-image": urlVal, "background-size": "cover", 
-"background-attachment": "fixed", "background-position": "center", "background-repeat": "no-repeat" });
+$("body").css({
+    "background-image": urlVal, "background-size": "cover",
+    "background-attachment": "fixed", "background-position": "center", "background-repeat": "no-repeat", "background-color": "rgba(0, 0, 0, 0.3)", "background-blend-mode": "overlay", "transition":" all 1s ease-in-out"
+});
 
