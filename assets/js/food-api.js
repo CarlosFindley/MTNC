edbc34e4b327bdb054f2c70f0a8c26bface58e72
nVar <nkeledjian@gmail.com>
2018-11-08 17:55:45 -0800

    setting up HTML elements to render

--- assets/js/food-api.js
+++ assets/js/food-api.js
@@ -14,7 +14,7 @@ $('#search-form').bind('keydown', function(e) {
 var food = {
   "async": true,
   "crossDomain": true,
-  "url": "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=food&location=" + myCity + "&limit=7",
+  "url": "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=food&location=" + myCity + "&limit=9",
   "method": "GET",
   "headers": {
     "cache-control": "no-cache",
@@ -27,15 +27,20 @@ $(document).ready(function(){
   $.ajax(food).done(function (response) {
     console.log(response);
     for (var i=0; i<response.businesses.length; i++) {
-      $("#food-api").append(`<div><img src="${response.businesses[i].image_url}"></div>`);
-      $("#food-api").append(`<div>${response.businesses[i].name}</div>`);
-      $("#food-api").append(`<div>Price: ${response.businesses[i].price}</div>`);
-      $("#food-api").append(`<div>Yelp Rating: ${response.businesses[i].rating}</div>`);
-      $("#food-api").append(`<div>${response.businesses[i].location.address1}</div>`);
-      $("#food-api").append(`<div>${response.businesses[i].display_phone}</div>`);
+      var yelpDiv = "<div>";
+
+      yelpDiv.attr("class", "row");
+
+      $("#yelp-data").append(`<div><img src="${response.businesses[i].image_url}"></div>`);
+      $("#yelp-data").append(`<div>${response.businesses[i].name}</div>`);
+      $("#yelp-data").append(`<div>Price: ${response.businesses[i].price}</div>`);
+      $("#yelp-data").append(`<div>Yelp Rating: ${response.businesses[i].rating}</div>`);
+      $("#yelp-data").append(`<div>${response.businesses[i].location.address1}</div>`);
+      $("#yelp-data").append(`<div>${response.businesses[i].display_phone}</div>`);
       // temporary styling
-      $("#food-api").append(`<div>${"-------------"}</div>`);
+      $("#yelp-data").append(`<div>${"-------------"}</div>`);
     }
+    
   });
 
 
