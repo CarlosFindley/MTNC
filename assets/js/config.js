9020984f5d9bec998157cf5175c7f1e2fa908c97
Nareg Keledjian <40443595+nkeledjian@users.noreply.github.com>
2018-11-09 20:05:41 -0800

    Delete config.js

--- assets/js/config.js
+++
@@ -1,4 +0,0 @@
-var config = {
-    yelpAPI : 'LE4KMSOx0I0X44b91OyrTxZ3WbQp8FtNYS60-mB2IKAWgmpgZx5TcQxTiFaljwtBNghQBYedUskebLAPKwlwCIAGKHIZwloSTikVM102TvI5DZ6sC1RyEBrJkSviW3Yx',
-    postman : "9bebf40a-c1a1-4c4f-b6bd-f7ac3fbe0da0",
-}
